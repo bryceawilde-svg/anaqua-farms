c2f5c7f15586cc3c491d7318c6958e3de347f00a
Bryce <bryceawilde@gmail.com>
2026-04-28 12:27:44 -0500

    Fix Supabase URL to correct project

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cglkvfsnfeqsrqeldxxj.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnbGt2ZnNuZmVxc3JxZWxkeHhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMzQ5MjAsImV4cCI6MjA5MDgxMDkyMH0.mVXZ_1tNHmaw9JLnCdiD-O82pqFlDlTpEv975oOSfTo'
+const supabaseUrl = 'https://mlxaljozizaarvdcssew.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1seGFsam96aXphYXJ2ZGNzc2V3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNTc0MjQsImV4cCI6MjA5MjYzMzQyNH0.P1neFr85TyCtuJOmc83bG_bApWnuE2oyNKtnNT_OZ2A'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
